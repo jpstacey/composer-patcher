db9b3db36f812f01b3e8e573d208074bbe2ef003
J-P <jp@dombey>
2014-08-19 20:59:26 +0100

    Switch to camelCase and start downloading patches

--- src/Composer/Patcher/PatcherPlugin.php
+++ src/Composer/Patcher/PatcherPlugin.php
@@ -8,6 +8,7 @@ use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
 use Composer\Script\Event;
+use Composer\Util\RemoteFileSystem;
 
 class PatcherPlugin implements PluginInterface
 {
@@ -29,7 +30,32 @@ class PatcherPlugin implements PluginInterface
     {
         // Obtain install path, so as to patch within it.
         $package = $event->getOperation()->getPackage();
-        $installation_manager = $event->getComposer()->getInstallationManager();
-        $install_path = $installation_manager->getInstaller($package->getType())->getInstallPath($package);
+        $manager = $event->getComposer()->getInstallationManager();
+        $installPath = $manager->getInstaller($package->getType())->getInstallPath($package);
+
+        // Get list of patch URLs from "extra": { "patch": [ ... ]}.
+        $extraData = $package->getExtra();
+        if (!isset($extraData["patch"]) || !count($extraData["patch"]))
+        {
+            return;
+        }
+
+        // Descend into install directory, loop over patches and apply.
+        $currentDir = getcwd();
+        chdir($installPath);
+        $downloader = new RemoteFilesystem($event->getIO(), $event->getComposer()->getConfig());
+        foreach ($extraData["patch"] as $patchUrl) {
+          self::getAndApplyPatch($downloader, $patchUrl);
+        }
+        chdir($currentDir);
+    }
+
+    /**
+     * Apply a remote patch on code in the current directory.
+     */
+    private static function getAndApplyPatch($downloader, $patchUrl)
+    {
+        $hostname = parse_url($patchUrl, PHP_URL_HOST);
+        $downloader->copy($hostname, $patchUrl, "/tmp/patch", TRUE);
     }
 }
