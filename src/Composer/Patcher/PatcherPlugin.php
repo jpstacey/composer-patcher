78837969551dabed8345237157472bb9d46bd39a
J-P <jp@dombey>
2014-08-19 16:41:09 +0100

    Add post-package-install script

--- src/Composer/Patcher/PatcherPlugin.php
+++ src/Composer/Patcher/PatcherPlugin.php
@@ -13,4 +13,8 @@ class PatcherPlugin implements PluginInterface
     {
         file_put_contents("/tmp/patcher.txt", "Yes");
     }
+    public function postPackageInstall(Event $event)
+    {
+        file_put_contents("/tmp/ppi.txt", "Yes");
+    }
 }
