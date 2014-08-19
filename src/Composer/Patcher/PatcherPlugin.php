54ef75f590a7cf5b41959cf2f7830545a59d5074
J-P <jp@dombey>
2014-08-19 17:49:18 +0100

    Getting package installation directory via installationManager

--- src/Composer/Patcher/PatcherPlugin.php
+++ src/Composer/Patcher/PatcherPlugin.php
@@ -11,12 +11,25 @@ use Composer\Script\Event;
 
 class PatcherPlugin implements PluginInterface
 {
+    /**
+     * Implements PluginInterface::activate().
+     *
+     * Currently does nothing: postPackageInstall() below does heavy lifting.
+     */
     public function activate(Composer $composer, IOInterface $io)
     {
-        file_put_contents("/tmp/patcher.txt", "Yes");
     }
+
+    /**
+     * Implements scripts class method postPackageInstall().
+     *
+     * After package install, interrogates patch JSON array and applies.
+     */
     public static function postPackageInstall(Event $event)
     {
-        file_put_contents("/tmp/ppi.txt", "Yes");
+        // Obtain install path, so as to patch within it.
+        $package = $event->getOperation()->getPackage();
+        $installation_manager = $event->getComposer()->getInstallationManager();
+        $install_path = $installation_manager->getInstaller($package->getType())->getInstallPath($package);
     }
 }
